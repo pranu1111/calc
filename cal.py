7970214e51252da7492668899e5ab6760ce9914e
Pranita kamble <107885176+pranu1111@users.noreply.github.com>
2023-09-08 13:14:28 +0530

    Add files via upload

---
+++ cal.py
@@ -0,0 +1,12 @@
+print("welcome to the program of calculator")
+"""print(5+10)
+print(10-10)
+print(5**3)
+print(5%3)"""
+a = 10
+b = 2
+print("addition of a and b is = ",a+b)
+print("subtraction of a nd b is = ", a-b)
+print("multiplication of a and b is =", a*b)
+print("division of a and b is =",a/b)
+print("modulu division of a and b is =",a%b)
